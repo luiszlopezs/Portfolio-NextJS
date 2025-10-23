4777c49e35fb7747ca74c7bc5f70a698c10a633c
Luis-Lopez <luflopezp@udistrital.edu.co>
2025-10-23 16:09:28 +0000

    feature: creacion y adiciones iniciales portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,183 @@
 import Image from "next/image";
 
 export default function Home() {
+  const name = "Luis";
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="mt-8">
+      <header
+        className="page-header"
+        id="inicio"
+        aria-label={`Titulo principal de la pagina: hola, soy ${name}`}
+      >
+        <h1 className="page-header__text text-blue-600">hola, soy {name}</h1>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+      <nav
+        className="nav-bar"
+        aria-label="barra de navegacion: sobre mi - proyectos y certificados - redes sociales"
+      >
+        <div className="nav-bar__text-container">
+          <a className="nav-bar__text" href="#inicio">
+            Inicio
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a className="nav-bar__text" href="#sobre-mi">
+            Sobre mi
+          </a>
+          <a className="nav-bar__text" href="#proyectos-y-cert">
+            Proyectos y Certificados
+          </a>
+          <a className="nav-bar__text" href="#redes-sociales">
+            Redes Sociales
           </a>
         </div>
+      </nav>
+      <main>
+        <section className="info-block" id="sobre-mi">
+          <h2 className="info-block__title">Acerca de Mi</h2>
+          <p className="info-block__paragraph bg-gray-100 p-4 rounded-lg">
+            Actualmente estudio Ingeniería de Sistemas y tengo 18 años. <br />
+            Me interesa aprender desarrollo frontend, por eso estoy tomando este
+            curso.
+            <br />
+            En mi tiempo libre me gusta ver series, películas, jugar fútbol y
+            más.
+          </p>
+        </section>
+        <section className="info-block info-block--feature-horizontal">
+          <article className="info-card" aria-label="Comidas que me gustan">
+            <h3 className="info-card__title">
+              Algunas de las comidas que me gustan
+            </h3>
+            <div className="info-block info-block--feature-img">
+              <img
+                className="img"
+                src="https://uk.ooni.com/cdn/shop/articles/20220211142645-margherita-9920_e41233d5-dcec-461c-b07e-03245f031dfe.jpg?v=1737105431&width=1080"
+                alt="pizza"
+              />
+              <img
+                className="img"
+                src="https://biteswithbri.com/wp-content/uploads/2021/02/HamburgerPattyRecipe04.jpg"
+                alt="hamburguesa"
+              />
+              <img
+                className="img"
+                src="https://i.pinimg.com/736x/a1/bc/32/a1bc32e5c4a8ae7f128da91aa8f4537b.jpg"
+                alt="ceviche"
+              />
+            </div>
+          </article>
+
+          <article className="info-card" aria-label="Mis hobbies">
+            <h3 className="info-card__title">Mis hobbies</h3>
+            <div className="info-block info-block--feature-img">
+              <img
+                className="img"
+                src="https://cdna.artstation.com/p/assets/images/images/051/783/186/large/touati-fellouh-021.jpg?1658166568"
+                alt="peliculas"
+              />
+              <img
+                className="img"
+                src="https://yt3.googleusercontent.com/W2KDWzfiuml1yad1e9utJiOJfb3YLimLTBqcZfUFKeiQmjdBfDAO-vr0WK9LRCMM6WTyK5BSo-0=s900-c-k-c0x00ffffff-no-rj"
+                alt="netflix"
+              />
+              <img
+                className="img"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Football_in_Bloomington%2C_Indiana%2C_1995.jpg/1200px-Football_in_Bloomington%2C_Indiana%2C_1995.jpg"
+                alt="futbol"
+              />
+            </div>
+          </article>
+        </section>
+
+        <section
+          className="estudio"
+          id="proyectos-y-cert"
+          aria-label="seccion de estudio"
+        >
+          <article className="estudioArticle" aria-label="Mis proyectos">
+            <h3 className="subtitle">Mis Proyectos</h3>
+
+            <section className="proyectos">
+              <article className="proyectoN">
+                <img />
+                <h4>Proyecto1</h4>
+              </article>
+              <article className="proyectoN">
+                <img />
+                <h4>Proyecto2</h4>
+              </article>
+              <article className="proyectoN">
+                <img />
+                <h4>Proyecto3</h4>
+              </article>
+              <article className="proyectoN">
+                <img />
+                <h4>Proyecto4</h4>
+              </article>
+            </section>
+          </article>
+          <div className="separador"></div>
+          <article
+            className="estudioArticle"
+            aria-label="mis certificados de ACM"
+          >
+            <h3 className="subtitle">Mis certificados de ACM↗</h3>
+            <div>
+              <a
+                href=" https://www.linkedin.com/in/luisz-lopezs-32946b347/overlay/1755223004665/single-media-viewer/?profileId=ACoAAFa-UhEBI7GjIdytxFVLag78f3ly5O75uUE"
+                target="_blank"
+              >
+                <img
+                  className="img"
+                  src="svgsLogos/python-127-svgrepo-com.svg"
+                  alt="Python"
+                />
+              </a>
+              <img
+                className="img"
+                src="svgsLogos/wait-time-svgrepo-com.svg"
+                alt="Comming Soon"
+              />
+            </div>
+          </article>
+        </section>
+        <section
+          className="redes-sociales"
+          id="redes-sociales"
+          aria-label="seccion de redes sociales"
+        >
+          <h3 className="subtitle">Mis redes sociales↗</h3>
+          <div className="logos-redes">
+            <a
+              href="https://www.linkedin.com/in/luisz-lopezs-32946b347/"
+              target="_blank"
+            >
+              <img
+                className="img"
+                src="svgsLogos/linkedin-svgrepo-com.svg"
+                alt="linkedin"
+              />
+            </a>
+            <a href="https://www.github.com/luiszlopezs" target="_blank">
+              <img
+                className="img"
+                src="svgsLogos/github-142-svgrepo-com.svg"
+                alt="github"
+              />
+            </a>
+            <a href="https://www.instagram.com/justhailen" target="_blank">
+              <img
+                className="img"
+                src="svgsLogos/instagram-svgrepo-com.svg"
+                alt="Instagram"
+              />
+            </a>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer aria-label="Footer - copyright">
+        <p> 2025 AboutMeLuis</p>
+        <p>Any questions? Dont hesitate to contact me.</p>
       </footer>
     </div>
   );
