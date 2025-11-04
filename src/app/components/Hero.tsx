c9f83267e32217088696304bd68e97e622f6664f
Luis-Lopez <luflopezp@udistrital.edu.co>
2025-11-04 14:46:06 +0000

    feature: empezar a implementar js

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,15 @@
 export default function Hero() {
+    const birthDate = new Date(2007, 9, 1); 
+    const today = new Date();
+    const age = today.getFullYear() - birthDate.getFullYear();
+
     return (
         <section className="text-center my-[50px]    flex flex-row justify-center items-center max-h-[500px] m-[40px] " id="hero">
             <div className="bg-[url('/image.png')] bg-contain bg-no-repeat bg-center w-[500px] h-[500px]"></div>
             <div className="max-w-[500px]">
                 <h2 className="text-[28px] font-bold text-[var(--blue-crayola)]">Acerca de Mi</h2>
                 <p >
-                    Actualmente estudio Ingeniería de Sistemas y tengo 18 años. <br />
+                    Actualmente estudio Ingeniería de Sistemas y tengo {age} años. <br />
                     Me interesa aprender desarrollo frontend, por eso estoy tomando este
                     curso.
                 </p>
---
+++ src/app/javasc.js
@@ -0,0 +1,4 @@
+let x = 5;
+for (;x > 0; x -= 1) {
+    console.log("x is positive");
+}
