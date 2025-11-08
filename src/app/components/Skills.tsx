5bbc4ea424c4d478307539b80d94e4f1f5605dab
hailen <soyhailen@gmail.com>
2025-11-07 20:28:48 -0500

    s

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -18,7 +18,7 @@ export default function Skills() {
                     </div>
                     ))}      
             </div>
-            </div>  
+            </div>   
         </div>
     );
 }
